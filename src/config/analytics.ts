338b978100a3afd336d6a37b069bb144c65dff7c
Tom Larcher <tom.larcher@gmail.com>
2024-11-03 11:55:49 +1100

    Added content to the `index.astro`, `featured.astro` and `now.astro` pages. Added new styling definitions to `base.css`. Added Google Analytics details via `analytics.ts`

---
+++ src/config/analytics.ts
@@ -0,0 +1,3 @@
+export const measurementId = "G-E9M0TDRYDZ"; // @note: Please update this value with your own Measurement ID.
+export const googleSiteVerification =
+  "9CTJ_PE2WthXUP7qyrbuTvoP60yB3K732zMYOI5PMJM"; // @note: Please update this value with your own Google Site Verification ID.
